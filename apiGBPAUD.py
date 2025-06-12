359646bbf1847103e426e8414089dc7e46430218
DanielaOrellanaS <orellanadaniela1111@gmail.com>
2025-06-12 17:18:37 -0500

    add new api for GBPAUD

--- apiGBPAUD.py
+++ apiGBPAUD.py
@@ -83,15 +83,51 @@ def predecir_registro(data: dict):
 
     # Predicción
     with torch.no_grad():
-        raw_output = model(x).item()
+        pred_raw = model(input_vector).item()
+    profit_pred = denormalize(pred_raw, min_max['min_profit'], min_max['max_profit'])
 
-    profit = denormalize(raw_output, min_profit, max_profit)
-    tipo = calcular_operacion(profit, MINIMO_GLOBAL)
-
-    print(f"Respuesta modelo: raw_output={raw_output}, profit={profit}, tipo={tipo}")
+    # Clasificación
+    if abs(profit_pred) > MINIMO_GLOBAL:
+        tipo = 'BUY' if profit_pred > 0 else 'SELL'
+    else:
+        tipo = 'NO_SIGNAL'
 
     return {
-        "raw_output": raw_output,
-        "profit_prediction": profit,
-        "tipo_operacion": tipo
+        "profit_prediction": round(profit_pred, 6),
+        "tipo_prediction": tipo
     }
+
+def map_get_params_to_registro(params):
+    registro = {
+        "fecha": params["fecha"].replace("T", " "),
+        "precioopen5": float(params["o5"]),
+        "precioclose5": float(params["c5"]),
+        "preciohigh5": float(params["h5"]),
+        "preciolow5": float(params["l5"]),
+        "volume5": float(params["v5"]),
+        "volume5_min": 200,
+        "volume5_max": 1200,
+        "precioopen15": float(params["o15"]),
+        "precioclose15": float(params["c15"]),
+        "preciohigh15": float(params["h15"]),
+        "preciolow15": float(params["l15"]),
+        "volume15": float(params["v15"]),
+        "volume15_min": 500,
+        "volume15_max": 3000,
+        "rsi5": float(params["r5"]) * 100,
+        "rsi15": float(params["r15"]) * 100,
+        "iStochaMain5": float(params["m5"]) * 100,
+        "iStochaSign5": float(params["s5"]) * 100,
+        "iStochaMain15": float(params["m15"]) * 100,
+        "iStochaSign15": float(params["s15"]) * 100,
+    }
+    return registro
+
+@app.get("/predict_get_with_file")
+async def predict_get(request: Request):
+    params = request.query_params
+    registro = map_get_params_to_registro(params)
+    resultado = predecir_registro(registro)  
+    return resultado
+
+
